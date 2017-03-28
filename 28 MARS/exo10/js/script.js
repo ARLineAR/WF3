6a7b30da8a1c07a8fef9028585ee377c368e0384
Etudiant <Etudiant>
2017-03-28 16:33:05 +0200

    Initiation à JS 2

---
+++ 28 MARS/exo10/js/script.js
@@ -0,0 +1,4 @@
+// Créer une fonction qui affiche 'Hello world' dans la console
+function helloWorld(){
+    console.log('Hello World !')
+};
---
+++ 28 MARS/exo9/js/script.js
@@ -0,0 +1,22 @@
+var user = 'Aline Rambier';
+
+// Créer une variable de type ARRAY (tableau)
+var myArray = [ 'Du texte', 1979, true, user ];
+
+// Afficher le tableau dans la console
+console.log(myArray);
+
+// Afficher la taille du tableau (nombre d'index)
+console.log( 'La taille du tableau est : ' + myArray.length );
+
+// Afficherun des index du tableau
+console.log( myArray[0] ); // =>'Du texte'
+console.log( myArray[2] ); // => true
+
+// Ajouter un index dans le tableau
+myArray.push( 'Une valeur en plus' );
+console.log( myArray );
+
+// Supprimer et remplacer des index du tableau
+myArray.splice( 2, 1, false );
+console.log( myArray );
