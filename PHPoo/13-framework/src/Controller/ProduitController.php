bcdbf8b3b97dc027c8955f99599fd8deb8242fd8
ARLineAR <alinerambier@gmail.com>
2017-06-08 09:44:16 +0200

    PHP OO fin

---
+++ PHPoo/13-framework/src/Controller/ProduitController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Controller;
+
+use Controller\Controller;
+
+class ProduitController extends Controller{
+    
+    public function afficheAll(){
+        $produits = $this -> getRepository() -> getAllProduits();
+        $categories = $this -> getRepository() -> getAllCategories();
+        
+        
+        $params= array(
+            'produits' => $produits,
+            'categories' => $categories,
+            'title' => 'Boutique de mon site'
+        );
+
+        return $this -> render('layout.html', 'boutique.html', $params); // pour afficher la vue
+        // require(__DIR__ . '/../View/Produit/boutique.php');
+
+
+    }
+
+    public function affiche($id){
+        $produit = $this -> getRepository() -> getProduitById($id);
+        $suggestions = $this -> getRepository() -> getAllSuggestions($produit['categorie'], $produit['id_produit']);
+        // $this -> render();
+
+        require(__DIR__ .'/../View/Produit/fiche_produit.php');
+        
+       
+    }
+
+    public function categorie($categorie){
+        $produits = $this -> getRepository() -> getAllProduitsByCategorie($categorie);
+        $categories = $this -> getRepository() -> getAllCategories();
+        // $this -> render();
+
+        require(__DIR__ .'/../View/Produit/categorie.php');
+        
+
+    }
+
+}
